9fcbfeb5a12b9ebea30bdb3999d73508b4beb2f4
ponotech-test01 <test01@ponotech.net>
2024-10-09 09:46:19 +0900

    nodes/__init__.pyを修正

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,7 +1,7 @@
 # my_custom_nodes/nodes/__init__.py
 
 # 個別のモジュールからクラスをインポート
-from .text_transformer import TextTransformerNode
+from .text_transformer_node import TextTransformerNode
 
 # このサブパッケージから他のスクリプトがインポート可能な要素を定義
 __all__ = ['TextTransformerNode']
